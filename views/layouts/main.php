09946e15ea4ee468ec6bf56980373fa609061c40
Андрей <koikov.andrey@gmail.com>
2020-08-21 15:39:46 +1000

    start with new main

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,60 +1,77 @@
+<!--<html>-->
+<?// use yii\bootstrap4\Nav;
+//use yii\bootstrap4\NavBar;
+//?>
+<? $this->beginPage();?><!-- Тригер для бутстрапа -->
+<!---->
+<!--        <head>-->
+<!--            <style>-->
+<!--                .my-link{-->
+<!--                    color: #050101;-->
+<!--                }-->
+<!--                .my-navbar {-->
+<!--                    background-color: #FFC90D;-->
+<!--                    border-color: #E7E1E7;-->
+<!---->
+<!--                }-->
+<!--                .my-class{-->
+<!--                    left: auto !important;-->
+<!--                    right: 0px;-->
+<!--                }-->
+<!--            </style>-->
+<!--            <title>Finance</title>-->
+<!--            --><?// $this->head();?><!--<  Тригер для бутстрапа -->
+<!--        </head>-->
+<!---->
+<!--        <body>-->
+<!--        --><?// $this->beginBody();?><!--  Тригер для бутстрапа -->
+<!--        --><?//
+//           NavBar::begin([
+//               'brandLabel' => '<a href="/"> <img src="/images/DNK.jpg." width="210" height="70" id="img1"  alt="ДОМ НАУЧНОЙ КОЛЛАБОРАЦИИ"/></a>',
+//                    'brandUrl'=> Yii::$app->homeUrl,
+//               'brandOptions' => ['class' => 'd-flex align-items-start'],
+//                    'options'=>
+//                    [
+//                            'class' => '  my-navbar my-navbar  navbar-expand-lg',
+//                    ],
+//           ]);
+//
+//           if (Yii::$app->user->isGuest)
+//           $menu =[
+//             ['label'=>'Join','url'=>['/user/join']],
+//               ['label'=>'Log in','url'=>['/user/login']],
+//           ];
+//           else
+//               $menu =[
+//                   ['label'=>Yii::$app->user->getIdentity()->name],
+//                   ['label'=>'Logout','url'=>['/user/logout']],
+//               ];
+//           echo Nav::widget([
+//                   'options'=>['class'=>'ml-auto'],
+//               'items'=>$menu
+//           ]);
+//            NavBar::end();
+//        ?>
+<!---->
+<!--        <div class="container" style="margin-top: 70px ">-->
+<!---->
+<!--        --><?//=$content?>
+<!---->
+<!--        </div>-->
+<!---->
+<!--        --><?// $this->endBody();?><!-- Тригер для бутстрапа -->
+<!--        </body>-->
+<!---->
+<!---->
+<!--</html>-->
+<?// $this->endPage();?><!--  Тригер для бутстрапа -->
 <html>
-<? use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
-?>
-<? $this->beginPage();?><!--  Тригер для бутстрапа -->
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>123</title>
+</head>
+<body>
 
-        <head>
-            <style>
-                .my-navbar {
-                    background-color: #FFC90D;
-                    border-color: #E7E1E7;
-                }
-            </style>
-            <title>Finance</title>
-            <? $this->head();?><!--  Тригер для бутстрапа -->
-        </head>
-
-        <body>
-        <? $this->beginBody();?><!--  Тригер для бутстрапа -->
-        <?
-           NavBar::begin([
-               'headerContent' => '<a href="/"> <img src="/images/DNK.jpg." width="210" height="70" id="img1"  alt="ДОМ НАУЧНОЙ КОЛЛАБОРАЦИИ"/></a>',
-                    'brandUrl'=> Yii::$app->homeUrl,
-                    'options'=>
-                    [
-
-                            'class' => 'my-navbar  navbar-fixed-top'
-                    ],
-           ]);
-
-           if (Yii::$app->user->isGuest)
-           $menu =[
-             ['label'=>'Join','url'=>['/user/join']],
-               ['label'=>'Log in','url'=>['/user/login']],
-           ];
-           else
-               $menu =[
-                   ['label'=>Yii::$app->user->getIdentity()->name],
-                   ['label'=>'Logout','url'=>['/user/logout']],
-               ];
-           echo Nav::widget([
-                   'options'=>['class'=>'navbar-nav navbar-right'],
-               'items'=>$menu
-           ]);
-            NavBar::end();
-        ?>
-
-        <div class="container" style="margin-top: 60px ">
-
-        <?=$content?>
-
-        </div>
-
-        <? $this->endBody();?><!--  Тригер для бутстрапа -->
-        </body>
-
-
+</body>
 </html>
-<? $this->endPage();?><!--  Тригер для бутстрапа -->
-
