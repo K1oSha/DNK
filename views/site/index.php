0651e00c77c9932d6e171f2d233bccc745247113
Андрей <koikov.andrey@gmail.com>
2020-08-22 14:22:55 +1000

    visual changes on / page

--- views/site/index.php
+++ views/site/index.php
@@ -1,48 +1,300 @@
 
 
 <div>
-<!--<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">-->
-<!--    <ol class="carousel-indicators">-->
-<!--        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>-->
-<!--        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>-->
-<!--        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>-->
-<!--    </ol>-->
-<!--    <div class="carousel-inner">-->
-<!--        <div class="carousel-item active">-->
-<!--            <img class="d-block w-100" src="/images/DNK.jpg." alt="First slide">-->
-<!--        </div>-->
-<!--        <div class="carousel-item">-->
-<!--            <img class="d-block w-100" src="/images/Безы32131мянный.jpg." alt="Second slide">-->
-<!--        </div>-->
-<!--        <div class="carousel-item">-->
-<!--            <img class="d-block w-100" src="/images/DNK.jpg." alt="Third slide">-->
+<div id="carouselExampleIndicators" class="carousel slide" >
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" ></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div style="background-color: #b9bc23" class="carousel-item active">
+            <img width="800" height="800" class="d-block w-75" src="/images/Andrianov.jpg." alt="First slide">
+            <h1>Адрианов Андрей Владимирович</h1>
+            <h4>Доктор биологических наук, академик РАН. Вице-
+                президент Российской академии наук (с 2017г.), научный руководитель Национального
+                научного центра морской биологии им. А.В. Жирмунского ДВО РАН, председатель
+                экспертного совета РАН, председатель Комитета РАН по Программе ООН по
+                окружающей среде (с 2018г.), почетный доктор ДВФУ (2018).</h4>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="/images/DNK_лого_2.jpg." alt="Third slide">
+            <h1>Описание ДНК</h1>
+            <h4>Дом научной коллаборации - это центр дополнительного образования для детей и
+                взрослых, включающий образовательные программы по освоению современных
+                прикладных и гибких компетенций. ДНК подразумевает новый подход к образованию,
+                реализующийся через выполнение учащимися реальных кейсов по передовым
+                направлениям развития технологий.</h4>
+        </div>
+
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+<!--<h1 style="text-align: left; font-weight: 900;">Описание ДНК</h1>-->
+<!--    <h3>Дом научной коллаборации - это центр дополнительного образования для детей и-->
+<!--        взрослых, включающий образовательные программы по освоению современных-->
+<!--        прикладных и гибких компетенций. ДНК подразумевает новый подход к образованию,-->
+<!--        реализующийся через выполнение учащимися реальных кейсов по передовым-->
+<!--        направлениям развития технологий.-->
+<!--    </h3>-->
+<!--    <div class="row" >-->
+<!--        <div class="col-lg-4 pull-left">-->
+<!--            <ol class="list-counter-square" style="max-width: 300px; margin: 0 auto;">-->
+<!--        <li>Проектная деятельность</li>-->
+<!--        <li>Реальные кейсы</li>-->
+<!--        <li>Научная ориентация</li>-->
+<!--        <li>Hard и Soft Skills</li>-->
+<!--        <li>Пространства для работы и отдыха</li>-->
+<!--        <li>Университетские мастерские и лаборатории</li>-->
 <!--        </div>-->
 <!--    </div>-->
-<!--    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">-->
-<!--        <span class="carousel-control-prev-icon" aria-hidden="true"></span>-->
-<!--        <span class="sr-only">Previous</span>-->
+<!--</div>-->
+    <br>
+    <br>
+    <div class="card w-100">
+        <div style="background-color:#FFC90D " class="card-body">
+            <h1 style="color: #000;" class="card-title">ДНК - это:</h1>
+            <H3 class="card-text">
+                ● Проектная деятельность<br>
+                ● Реальные кейсы<br>
+                ● Научная ориентация<br>
+                ● Hard и Soft Skills<br>
+                ● Пространства для работы и отдыха<br>
+                ● Университетские мастерские и лаборатории
+            </H3>
+            <h2>и ТЫ можешь стать частью этого!</h2>
+            <a href="#" class="btn btn-dark">Подать заявку</a>
+        </div>
+    </div>
+    <br>
+    <br>
+<!--<div class="list-group">-->
+<!--    <a href="#" class="list-group-item list-group-item-action flex-column align-items-start active">-->
+<!--        <div class="d-flex w-100 justify-content-between">-->
+<!--            <h5 class="mb-1">List group item heading</h5>-->
+<!--            <small>3 days ago</small>-->
+<!--        </div>-->
+<!--        <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>-->
+<!--        <small>Donec id elit non mi porta.</small>-->
 <!--    </a>-->
-<!--    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">-->
-<!--        <span class="carousel-control-next-icon" aria-hidden="true"></span>-->
-<!--        <span class="sr-only">Next</span>-->
+<!--    <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">-->
+<!--        <div class="d-flex w-100 justify-content-between">-->
+<!--            <h5 class="mb-1">List group item heading</h5>-->
+<!--            <small class="text-muted">3 days ago</small>-->
+<!--        </div>-->
+<!--        <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>-->
+<!--        <small class="text-muted">Donec id elit non mi porta.</small>-->
+<!--    </a>-->
+<!--    <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">-->
+<!--        <div class="d-flex w-100 justify-content-between">-->
+<!--            <h5 class="mb-1">List group item heading</h5>-->
+<!--            <small class="text-muted">3 days ago</small>-->
+<!--        </div>-->
+<!--        <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>-->
+<!--        <small class="text-muted">Donec id elit non mi porta.</small>-->
 <!--    </a>-->
 <!--</div>-->
-<h1 style="text-align: left; font-weight: 900;">Описание ДНК</h1>
-    <h3>Дом научной коллаборации - это центр дополнительного образования для детей и
-        взрослых, включающий образовательные программы по освоению современных
-        прикладных и гибких компетенций. ДНК подразумевает новый подход к образованию,
-        реализующийся через выполнение учащимися реальных кейсов по передовым
-        направлениям развития технологий.
-    </h3>
-    <div class="row" >
-        <div class="col-lg-4 pull-left">
-            <ol class="list-counter-square" style="max-width: 300px; margin: 0 auto;">
-        <li>Проектная деятельность</li>
-        <li>Реальные кейсы</li>
-        <li>Научная ориентация</li>
-        <li>Hard и Soft Skills</li>
-        <li>Пространства для работы и отдыха</li>
-        <li>Университетские мастерские и лаборатории</li>
+<div>
+    <h1 style="text-align: center">Описание образовательных программ</h1>
+    <br>
+    <h3 style="text-align: center">Образовательное направление “Детский университет” (5 - 9 классы)</h3>
+<div class="card-group">
+    <div class="card">
+        <img class="card-img-top" src="..." alt="VR">
+        <div class="card-body">
+            <h5 class="card-title">VR</h5>
+            <p class="card-text">
+                Программа подразумевает комплексный подход к освоению VR технологии, где
+                обучающиеся смогут развить свои навыки программирования, 3D дизайна и получат опыт
+                командной работы в рамках проектной деятельности.
+            </p>
+            <p class="card-text"><small class="text-muted">
+                    Преподаватель: Ян Татьяна Вячеславовна<br>
+                    Возраст учащихся: 14-17 лет (8-9 класс)<br>
+                    Режим занятий: 1,5 часа два раза в неделю<br>
+                    Срок обучения: 1 полугодие
+                </small></p>
+        </div>
+    </div>
+    <div class="card">
+        <img class="card-img-top" src="..." alt="Космическое бюро">
+        <div class="card-body">
+            <h5 class="card-title">Космическое бюро</h5>
+            <p class="card-text">
+                Программа направлена на формирование навыков использования космических
+                снимков для реализации проектов школьников, опыта и механизмов взаимодействия с
+                операторами космических аппаратов Дистанционного зондирования Земли.
+            </p>
+            <p class="card-text"><small class="text-muted">
+                    Преподаватель: Храмцова Анастасия Владимировна<br>
+                    Возраст учащихся: 14-17 лет (8-9 класс)<br>
+                    Режим занятий: 1,5 часа два раза в неделю<br>
+                    Срок обучения: 1 год
+                </small></p>
+        </div>
+    </div>
+    <div class="card">
+        <img class="card-img-top" src="..." alt="Создание моделей ракет">
+        <div class="card-body">
+            <h5 class="card-title">Создание моделей ракет</h5>
+            <p class="card-text">
+                Программа направлена на освоение Hard- и Soft-компетенций обучающимися в
+                области урбанистики и аэротехнологий через использование проектного мышления.
+            </p>
+            <p class="card-text"><small class="text-muted">
+                    Преподаватель: Храмцова Анастасия Владимировна<br>
+                    Возраст учащихся: 14-17 лет (8-9 класс)<br>
+                    Режим занятий: 1,5 часа два раза в неделю<br>
+                    Срок обучения: 1 полугодие
+                </small></p>
+        </div>
+    </div>
+</div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="card-group">
+        <div class="card">
+            <img class="card-img-top" src="..." alt="Введение в космологию и основы космического инжиниринга">
+            <div class="card-body">
+                <h5 class="card-title">Введение в космологию и основы космического инжиниринга</h5>
+                <p class="card-text">
+                    Программа направлена на освоение компетенций программирования,
+                    прототипирования и конструирования в области космологии и космической инженерии.
+                </p>
+                <p class="card-text"><small class="text-muted">
+                        Преподаватель: Баранчугов Илья Александрович<br>
+                        Возраст учащихся: 10-14 лет (5-7 класс)<br>
+                        Режим занятий: 1,5 часа два раза в неделю<br>
+                        Срок обучения: 1 полугодие
+                    </small></p>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="..." alt="Космическое бюро">
+            <div class="card-body">
+                <h5 class="card-title">Робот Arduino-Raspberry-Mecanum</h5>
+                <p class="card-text">
+                    Программа направлена на формирование навыков по использованию симулятора
+                    CoppeliaSim для выполнения домашнего задания и для выполнения тестов, является
+                    модулем программы связанной с конструированием робота (позволяет лучше
+                    обучающимся понимать происходящие в конструкции робота процессы).
+                </p>
+                <p class="card-text"><small class="text-muted">
+                        Преподаватель: Ипатов Артём Александрович<br>
+                        Возраст учащихся: 10-14 лет (5-7 класс)<br>
+                        Режим занятий: 1,5 часа два раза в неделю<br>
+                        Срок обучения: 1 полугодие
+                    </small></p>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="..." alt="Создание моделей ракет">
+            <div class="card-body">
+                <h5 class="card-title">Интеллектуальные системы и технологии</h5>
+                <p class="card-text">
+                    Программа формирует компетенции, которые позволят обучающимся
+                    в будущем успешно создавать собственные электронные устройства, заниматься
+                    разработкой программного обеспечения, программированием микроконтроллеров, а также
+                    конкурировать на рынке рабочей силы в области информационных технологий.
+                </p>
+                <p class="card-text"><small class="text-muted">
+                        Преподаватель: Стрекалёв Вячеслав Олегович<br>
+                        Возраст учащихся: 14-17 лет (8-9 класс)<br>
+                        Режим занятий: 1,5 часа два раза в неделю<br>
+                        Срок обучения: 1 полугодие
+                    </small></p>
+            </div>
+        </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="card-group">
+        <div class="card">
+            <img class="card-img-top" src="..." alt="Промышленный дизайн">
+            <div class="card-body">
+                <h5 class="card-title">Промышленный дизайн</h5>
+                <p class="card-text">
+                    Программа направлена на освоение основ 3D моделирования и инженерной
+                    графики, развитие творческого и проектного мышления подготовка к участию в
+                    проектных и творческих выставках.
+                </p>
+                <p class="card-text"><small class="text-muted">
+                        Преподаватель: Гаврик Василиса Вячеславовна<br>
+                        Возраст учащихся: 10-14 лет (5-7 класс)<br>
+                        Режим занятий: 1,5 часа два раза в неделю<br>
+                        Срок обучения: 1 полугодие
+                    </small></p>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="..." alt="Основы человеко-машинных интерфейсов">
+            <div class="card-body">
+                <h5 class="card-title">Основы человеко-машинных интерфейсов</h5>
+                <p class="card-text">
+                    Программа направлена на освоение hard- и soft- компетенций обучающимися в
+                    области нейротехнологий через использования кейсового подхода.
+                </p>
+                <p class="card-text"><small class="text-muted">
+                        Преподаватель: Безрукавая Полина Андреевна<br>
+                        Возраст учащихся: 14-17 лет (8-9 класс)<br>
+                        Режим занятий: 1,5 часа два раза в неделю<br>
+                        Срок обучения: 1 год
+                    </small></p>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="..." alt="Биотехнологии">
+            <div class="card-body">
+                <h5 class="card-title">Биотехнологии</h5>
+                <p class="card-text">
+                    Программа направлена на формирование инженерно-биологического мышления у
+                    современных школьников, развитие естественного интереса к познанию, выстраивание
+                    личной и командной истории успеха.
+                </p>
+                <p class="card-text"><small class="text-muted">
+                        Преподаватели: Маськин Егор Викторович и Гребёнкин Павел Вячеславович<br>
+                        Возраст учащихся: 10-14 лет (5-7 класс) и 14-17 лет (8-9 класс)<br>
+                        Режим занятий: 1,5 часа два раза в неделю<br>
+                        Срок обучения: 1 год
+                    </small></p>
+            </div>
         </div>
     </div>
 </div>
