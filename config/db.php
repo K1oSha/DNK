8e1b574a2b831de61a4eb037695ed0a2ab653919
Pavel <k1osha@derpy.ru>
2020-08-31 20:49:42 +1000

    db fix

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
     return [
         'class'=>'\yii\db\Connection',
-        'dsn' => 'mysql:host=localhost;dbname=dnk',
-        'username' => 'u0831054_console',
+        'dsn' => 'mysql:host=localhost;dbname=u0831054_product',
+        'username' => 'u0831054_product',
         'password' =>'turquiose_distribution',
     ];
